d503cbca8916103da149bcead2aa01ac325556ef
Jo3ranDono <102674447+Jo3ranDono@users.noreply.github.com>
2022-05-19 14:20:26 +0300

    Delete Bytes and Big Integers.py.py

--- CryptoHack/Introduction_to_CryptoHack/Bytes and Big Integers.py.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from Crypto.Util.number import *
-
-cipher = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
-
-uncipher = cipher.to_bytes(33, "big")
-print(uncipher)
